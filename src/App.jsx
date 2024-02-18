e71b23750b0d38d1e952a18f7a22ab9cecaf0836
smdekate <smdekate@gmail.com>
2024-02-18 18:18:59 +0530

    configure redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
 import conf from './conf/conf';
+import { Header, Footer } from "./components"
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { login, logout } from './store/authSlice'
+import authService from './appwrite/auth'
 import './App.css'
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
-  console.log("appwrite url:", conf.appwriteURL);
-  return (
-    <>
-      <h1>App is working...</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData)
+          dispatch(login(userData))
+        else
+          dispatch(logout())
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  
+  return !loading ? (
+    <div className='min-height-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* TODO <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App
